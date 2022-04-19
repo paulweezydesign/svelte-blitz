65f1fd303583ae986f20c11f4dd20d036a0599ff
paulweezydesign <86060999+paulweezydesign@users.noreply.github.com>
2022-04-19 19:07:36 -0400

    Initial commit

---
+++ App.svelte
@@ -0,0 +1,24 @@
+<script>
+</script>
+
+<style>
+  body html {
+    font-family: "Fira Sans", sans-serif;
+  }
+</style>
+<nav class="grid grid-cols-2 items-center h-[100px] w-[100vw]">
+  <div class="">
+    <h1 class="text-2xl font-bold ml-[12%]">Svelte</h1>
+  </div>
+  <div class="flex justify-end ">
+    <a href="/">Home</a>
+    <a href="/about">About</a>
+    <a href="/contact">Contact</a>
+    <a href="/blog">Blog</a>
+  </div>
+</nav>
+<main>
+	<h1>Hello CodeSandbox</h1>
+	<h2>Start editing to see some magic happen!</h2>
+
+</main>
