2e4b51fbb2aed5b384bd8f8197dcd26e58827784
paulweezydesign <86060999+paulweezydesign@users.noreply.github.com>
2022-04-19 19:17:38 -0400

    adding a responsive navbar component

--- App.svelte
+++ App.svelte
@@ -6,19 +6,20 @@
     font-family: "Fira Sans", sans-serif;
   }
 </style>
-<nav class="grid grid-cols-2 items-center h-[100px] w-[100vw]">
+<nav class="bg-[#222] text-white/90 grid grid-cols-2 items-center h-[100px] w-[100vw]">
   <div class="">
     <h1 class="text-2xl font-bold ml-[12%]">Svelte</h1>
   </div>
-  <div class="flex justify-end ">
-    <a href="/">Home</a>
-    <a href="/about">About</a>
-    <a href="/contact">Contact</a>
-    <a href="/blog">Blog</a>
+  <div class="flex justify-end mr-[12%] ">
+    <a class="ml-7" href="/">Home</a>
+    <a class="ml-7" href="/about">About</a>
+    <a class="ml-7" href="/contact">Contact</a>
+    <a class="ml-7" href="/blog">Blog</a>
   </div>
 </nav>
 <main>
-	<h1>Hello CodeSandbox</h1>
-	<h2>Start editing to see some magic happen!</h2>
-
+  <div class="container mx-auto ">
+	<h1 class="text-center font-black text-6xl mt-8 ">Hello CodeSandbox</h1>
+	<h2 class="mt-3 text-center font-light text-3xl">Start editing to see some magic happen!</h2>
+  </div>
 </main>
